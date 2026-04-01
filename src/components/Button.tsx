535893ad200139e64b78c94b4fbeaef9e52f34d1
Soup Cat <soupcat.py@gmail.com>
2026-03-31 20:05:30 -0600

    reformatted button component
    now it's more efficient and practical
    - removed the <button> part
    - added a `local` parameter to determine whether to use target _blank or
    _self (it's false by default)

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,29 +1,22 @@
 import type {ReactNode} from "react";
 
 type ButtonProps = {
-    href?: string,
-    section?: boolean
     primary?: boolean,
     label?: string,
-    icon?: ReactNode
+    icon?: ReactNode,
+    href?: string,
+    local?: boolean
 }
 
-export default function Button({href, section = false, primary = false, label, icon}: ButtonProps) {
-
-    // define the stuff inside the button conditionally
-    const content = (<>
-        {label && <span>{label}</span>}
-        {icon && <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="CurrentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">{icon}</svg>}
-    </>)
-
-    // classes to be reused
-    const classes = `btn ${primary ? "btn-primary" : "btn-norm"} d-flex flex-row p05 g025 nodeco`
-
-    // determine whether to make an anchor or a button
-    if (href) {
-        return <a href={href} target={section? "_self" : "_blank"} className={classes}>{content}</a>
-    }
-    else {
-        return <button className={classes}>{content}</button>
-    }
+export default function Button({href, primary = false, local = false, label, icon}: ButtonProps) {
+    return (
+        <a
+            className={`btn ${primary ? "btn-primary" : "btn-norm"} d-flex flex-row p05 g025 nodeco`}
+            href={href}
+            target={local ? "_self" : "_blank"}
+        >
+            {label && <span>{label}</span>}
+            {icon && <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="CurrentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">{icon}</svg>}
+        </a>
+    )
 }
