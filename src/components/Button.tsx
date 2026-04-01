090601dc32144fb9137cca904d690da5a4ac7cea
Soup Cat <soupcat.py@gmail.com>
2026-03-31 22:14:55 -0600

    tweaked button SVGs

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -16,7 +16,14 @@ export default function Button({href, primary = false, local = false, label, ico
             target={local ? "_self" : "_blank"}
         >
             {label && <span>{label}</span>}
-            {icon && <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="CurrentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">{icon}</svg>}
+            {icon && <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24" height="24" viewBox="0 0 24 24"
+                fill="none" stroke="CurrentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"
+                className="radius-none"
+            >
+                {icon}
+            </svg>}
         </a>
     )
 }
