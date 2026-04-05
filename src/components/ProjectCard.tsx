a69df2346b1c8ebe94b8614dad2b760529d2cb20
Soup Cat <soupcat.py@gmail.com>
2026-04-04 18:41:15 -0600

    tweaked projects section
    - changed layout from grid to flex with wrapping
    - fixed the image's border radius affecting the entire document
    - modified how links work to fix "visit" links and simplify "repo" links

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -11,18 +11,18 @@ type ProjectCardProps = {
 
 export default function ProjectCard({title, description, image, link, repo}: ProjectCardProps) {
     return (
-        <article className="d-flex flex-col bg-bg border-primary radius-card">
+        <article className={`d-flex flex-col bg-bg border-primary radius-card ${styles.container}`}>
             <div className="d-flex flex-col align-start justify-center rel">
-                <h3 className={`color-primary m0 abs ${styles.title}`}>{title}</h3>
-                <img className={styles.image} src={`/src/images/${image}.png`} alt={image}/>
+                <h3 className={`color-primary m0 abs ${styles.title} ${styles.topRounded}`}>{title}</h3>
+                <img className={`${styles.topRounded} ${styles.image}`} src={`/src/images/${image}.png`} alt={image}/>
             </div>
             <div className="d-flex flex-col align-start justify-start p2 g2">
-                <span className={`body color-text ${styles.desc}`}>
+                <span className="body color-text">
                     {description}
                 </span>
                 <div className="d-flex flex-row align-center justify-start g1">
                     <Button
-                        href={link}
+                        href={`https://${link}`}
                         label="visit"
                         icon={<>
                             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
@@ -31,7 +31,7 @@ export default function ProjectCard({title, description, image, link, repo}: Pro
                         </>}
                     />
                     <Button
-                        href={repo}
+                        href={`https://github.com/${repo}`}
                         label="repo"
                         icon={
                             <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"/>
--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -3,25 +3,25 @@ import styles from "./Projects.module.css";
 
 export default function Projects () {
     return (
-        <section id="projects" className={`bg - bg d-flex flex-col align-center justify-start g6 ${styles.main}`}>
+        <section id="projects" className={`bg-bg bg d-flex flex-col align-center justify-start ${styles.main}`}>
             <div className="d-flex flex-col align-center justify-center g1">
                 <h1 className="color-primary m0">My Projects</h1>
-                <h3 className="color-text m0">kjsdh klsjadhf kljasdf lkjhasdkljhasdfjkhlasdfl kjh</h3>
+                <h3 className="color-text m0">All the stuff I've made! It's all open-source too :D</h3>
             </div>
-            <div className={`d-flex flex-row ${styles.wrapper}`}>
+            <div className={`d-flex flex-row justify-center p2 ${styles.wrapper}`}>
                 <ProjectCard
                     title="NMS Leyline Calculator"
-                    description="A web-based tool to find 3-star deposits in No Man's Sky. trying to make this multiline"
+                    description="A web-based tool to find 3-star deposits in No Man's Sky."
                     image="nmslc"
                     link="nmscd.com/leylinecalc"
-                    repo="github.com/nmscd/leylinecalc"
+                    repo="nmscd/leylinecalc"
                 />
                 <ProjectCard
                     title="Zsh Cheat Sheet"
                     description="A simple reference to all the most common zshell commands."
                     image="zshcheatsheet"
                     link="zshcheatsheet.vercel.app"
-                    repo="github.com/SoupCat-Py/codesheet2"
+                    repo="SoupCat-Py/codesheet2"
                 />
             </div>
         </section>
