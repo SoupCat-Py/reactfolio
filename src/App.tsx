6f4211865d4ed674372805866dbb767948779d8a
Soup Cat <soupcat.py@gmail.com>
2026-04-26 21:42:08 -0600

    replaced SVGs with inline ones
    it took so long to hand-make them lol

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,36 @@
 import Hero from "./sections/Hero.tsx";
 import Projects from "./sections/Projects.tsx";
-import sep1 from "./images/sep1.svg";
-import sep2 from "./images/sep2.svg";
 
 export default function App() {
     return (
-        <div className="d-flex flex-col g2">
+        <div className="d - flex flex-col g2">
+
             <Hero/>
-            <img src={sep1} alt="sep" className="zmax"/>
+
+            <svg xmlns="http://www.w3.org/2000/svg"
+                 viewBox="0 0 100 30"
+                 className="bg-none zmax"
+            >
+              <path fill="#dbc13b" d="m0 10 35-7 15 9 12-6 13 5 15-9 10 5v22H0V15z"/>
+              <path fill="#65a657" d="m0 10 30 3 32-3 15 7 23-5v17H0V15z"/>
+              <path fill="#007d6d" d="m0 10 14 1 17 8 19-4 12 2 12-3 15-2 11 3v14H0z"/>
+              <path fill="#004f60" d="m0 20 14-3 17 7 19-5 22 3 14-4 14 3v8H0z"/>
+              <path fill="#152435" d="m0 25 14-2 21-1 15 5 12-3 21 2 17-2v6H0z"/>
+            </svg>
+
             <Projects/>
-            <img src={sep2} alt="sep" className="zmax"/>
+
+            <svg xmlns="http://www.w3.org/2000/svg"
+                 viewBox="0 0 100 30"
+                 className="bg-none zmax"
+            >
+              <path fill="#dbc13b" d="m0 14 15-1 17-5 15 4 15 2 15-8 13 6 10 2v16H0z"/>
+              <path fill="#65a657" d="m0 14 17 2 15-2 10-1 18 4 8 1 15-4 17 2v14H0z"/>
+              <path fill="#007d6d" d="m0 16 14 4 17-4 16 1 12 3 14-4 15 1 12 3v10H0z"/>
+              <path fill="#004f60" d="m0 20 14-1 12 2 17-2 15 5 17-2 12-2 13 2v8H0z"/>
+              <path fill="#152435" d="m0 25 14-2 21-1 12 3 12-3 21 2 20-2v8H0z"/>
+            </svg>
+
         </div>
     )
 }
