df786d04ea203d5999d50d53cfd634baeddea40e
SoupCat <soupcat.py@gmail.com>
2026-05-04 11:06:05 -0600

    added some stuff to the footer
    might modify it later and still need to fix that weird blue line

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,9 +1,35 @@
+import styles from "./Footer.module.css"
+import type { ReactNode } from "react"
+
+type LinkProps = {
+  href: string;
+  children: ReactNode;
+}
+
+const Link = ({href, children}: LinkProps) => {
+  return(
+    <a href={`https://${href}`} target="_blank" className={styles.link}>
+      {children}
+    </a>
+  )
+}
+
 export default function Footer() {
   return (
-    <section className="bg-primary color-bg d-flex flex-col p2 g1">
-      <p>hi</p>
-      <p>there</p>
-      <p>:)</p>
+    <section className="bg-primary color-bg d-flex flex-col justify-start align-start p2 g025">
+      <p>
+        Most icons from&nbsp;
+        <Link href="https://tabler.io/icons">Tabler</Link>
+        &nbsp;and&nbsp;
+        <Link href="https://svgrepo.com">SVG Repo</Link>
+      </p>
+      <p>
+        Fonts from&nbsp;
+        <Link href="https://fonts.google.com">Google Fonts</Link>
+      </p>
+      <p>
+        This website is open-source too! → <Link href="github.com/SoupCat-Py/reactfolio">GitHub Repo</Link>
+      </p>
     </section>
   )
 }
