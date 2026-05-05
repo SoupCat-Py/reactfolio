368ac1a7b23357c0f0f4ee2b7dc4d150c8ebf149
SoupCat <soupcat.py@gmail.com>
2026-05-05 17:56:47 -0600

    changed footer content

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,5 +1,5 @@
-import styles from "./Footer.module.css"
-import type { ReactNode } from "react"
+import styles from "./Footer.module.css";
+import type { ReactNode } from "react";
 
 type LinkProps = {
   href: string;
@@ -16,20 +16,24 @@ const Link = ({href, children}: LinkProps) => {
 
 export default function Footer() {
   return (
-    <section className="bg-primary color-bg d-flex flex-col justify-start align-start p2 g025">
-      <p>
-        Most icons from&nbsp;
-        <Link href="https://tabler.io/icons">Tabler</Link>
-        &nbsp;and&nbsp;
-        <Link href="https://svgrepo.com">SVG Repo</Link>
-      </p>
-      <p>
-        Fonts from&nbsp;
-        <Link href="https://fonts.google.com">Google Fonts</Link>
-      </p>
-      <p>
-        This website is open-source too! → <Link href="github.com/SoupCat-Py/reactfolio">GitHub Repo</Link>
-      </p>
+    <section className="bg-primary color-bg d-flex flex-col justify-start align-start p2 g1">
+      <h2 className="color-bg m0">Built by Soup</h2>
+      <div className="d-flex flex-col justify-start align-start g025">
+        <p>
+          Most icons from&nbsp;
+          <Link href="tabler.io/icons">Tabler</Link>
+          &nbsp;and&nbsp;
+          <Link href="svgrepo.com">SVG Repo</Link>
+        </p>
+        <p>
+          Fonts from&nbsp;
+          <Link href="fonts.google.com">Google Fonts</Link>
+        </p>
+        <p>
+          © 2026 Soup Cat -&nbsp;
+          <Link href="github.com/SoupCat-Py/reactfolio/">GitHub Repo</Link>
+        </p>
+      </div>
     </section>
   )
 }
