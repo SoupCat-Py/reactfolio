7dd58cb411b8ea7fb895f7d3762545ef0bcda143
SoupCat <soupcat.py@gmail.com>
2026-05-05 17:56:41 -0600

    tweaked bottom separator

--- src/App.tsx
+++ src/App.tsx
@@ -31,11 +31,10 @@ export default function App() {
       <Contact/>
 
       <Separator paths={<>
-        <path fill="#152435" d="M0 10 35 0l15 12 12-6 13 5 15-9 10 5v23H0V15z"/>
-        <path fill="#004f60" d="m0 10 30 3 32-3 15 7 23-5v18H0V15z"/>
-        <path fill="#007d6d" d="m0 10 14 1 17 8 19-4 12 2 12-3 15-2 11 3v15H0z"/>
-        <path fill="#65a657" d="m0 20 14-3 17 7 19-5 22 3 14-4 14 3v9H0z"/>
-        <path fill="#dbc13b" d="m0 25 14-2 21-1 15 5 12-3 21 2 17-2v6H0z"/>
+        <path fill="#004f60" d="M0 10 35 0l15 12 12-6 13 5 15-9 10 5v23H0V15z"/>
+        <path fill="#007d6d" d="m0 10 30 3 32-3 15 7 23-5v18H0V15z"/>
+        <path fill="#65a657" d="m0 10 14 1 17 8 19-4 12 2 12-3 15-2 11 3v15H0z"/>
+        <path fill="#dbc13b" d="m0 20 14-3 17 7 19-5 22 3 14-4 14 3v9H0z"/>
       </>}/>
 
       <Footer/>
